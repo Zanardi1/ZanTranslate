d2499d8134ae112dc691b3127e9d867afe4a78d2
Bogdan Doicin <alex.zanardi@gmail.com>
2020-08-27 11:14:37 +0300

    Am scris engine-ul de traducere.

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from googletrans import Translator
 
+f = open('source.txt', 'r')
+sentence = f.read()
+f.close()
+
 translator = Translator()
-text = translator.translate('Hello', src='en', dest='ro')
+text = translator.translate(sentence, src='nl', dest='ro')
+
 print(text.text)
